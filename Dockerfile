85211f5c80375a75f550e4246fa28d4a2144217b
Robin Martijn <ik@robinmartijn.nl>
2022-02-22 15:29:37 +0100

    Final project

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,10 @@
 FROM node:latest
 
+LABEL version=0.1.0
+LABEL maintainer="Robin Martijn"
+
+ENV NAME=STIR
+
 COPY . /app
 
 WORKDIR /app
--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const logger = require('node-color-log')
+const express = require('express')
 
-logger.bgColor('green')
-    .color('white')
-    .log('Hello, world!');
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hi there!')
+
+    logger.bgColor('green')
+        .color('white')
+        .log('Hello, ' + process.env.NAME);
+})
+
+app.listen(port, () => {
+    console.log('Listening has started')
+})
